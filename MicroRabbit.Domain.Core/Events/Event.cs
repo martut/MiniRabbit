e0f60999fa4b78c992e79ce4e4a858d037eb1078
Marcin Wirzeski <40631628+martut@users.noreply.github.com>
2024-03-21 15:19:22 +0100

    implemented interfaces and classes for core

---
+++ MicroRabbit.Domain.Core/Events/Event.cs
@@ -0,0 +1,11 @@
+namespace MicroRabbit.Domain.Core.Events;
+
+public abstract class Event
+{
+    public DateTime Timestamp { get; protected set; }
+
+    protected Event()
+    {
+        Timestamp = DateTime.Now;
+    }
+}
